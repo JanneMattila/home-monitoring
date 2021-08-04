8f1c114e0f5cf6e8a393942f230ea252fd935df3
Janne Mattila <janne.mattila@hotmail.fi>
2021-08-04 19:30:32 +0300

    Add project placeholders

---
+++ src/HomeMonitoring/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+using System;
+using System.Threading.Tasks;
+
+namespace HomeMonitoring
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Home Monitoring started");
+            using var openTelemetry = Sdk.CreateTracerProviderBuilder()
+                .AddSource("HomeMonitoring")
+                .AddConsoleExporter()
+                .Build();
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+#if DEBUG
+                .AddUserSecrets<Program>()
+#endif
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            var configuration = builder.Build();
+
+            var navigateUri = configuration.GetValue<string>("navigateUri");
+
+            var navigator = new SensorScanner();
+            await navigator.ScanAsync(navigateUri);
+        }
+    }
+}
---
+++ src/HomeMonitoring/SensorScanner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HomeMonitoring
+{
+    public class SensorScanner
+    {
+        private readonly ActivitySource _source = new("SensorScanner");
+
+        public async Task ScanAsync(string navigateUri)
+        {
+            using var activity = _source.StartActivity("ScanAsync");
+
+            while (true)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+    }
+}
