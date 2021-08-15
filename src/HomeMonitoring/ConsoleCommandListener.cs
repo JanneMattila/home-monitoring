3808551ede117a9fe8313e43e9f1d574a9918c26
Janne Mattila <janne.mattila@hotmail.fi>
2021-08-15 14:37:32 +0300

    Add simplified sensor reporting to console output

--- src/HomeMonitoring/ConsoleCommandListener.cs
+++ src/HomeMonitoring/ConsoleCommandListener.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Linq;
 using ZigBeeNet;
+using ZigBeeNet.ZCL.Clusters;
 using ZigBeeNet.ZCL.Clusters.General;
+using ZigBeeNet.ZCL.Protocol;
 
 namespace HomeMonitoring
 {
@@ -10,6 +13,41 @@ namespace HomeMonitoring
         {
             if (command is ReportAttributesCommand reportAttributesCommand)
             {
+                if (reportAttributesCommand.ClusterId == ZclPressureMeasurementCluster.CLUSTER_ID)
+                {
+                    var measuredValue = reportAttributesCommand.Reports.FirstOrDefault(
+                        o => o.AttributeIdentifier == ZclPressureMeasurementCluster.ATTR_SCALEDVALUE);
+                    if (measuredValue != null && measuredValue.AttributeDataType.DataType == DataType.SIGNED_16_BIT_INTEGER)
+                    {
+                        
+                        var value = Convert.ToDouble(measuredValue.AttributeValue);
+                        Console.WriteLine($"{DateTime.Now} {value / 100} kPa");
+                        return;
+                    }
+                }
+                else if (reportAttributesCommand.ClusterId == ZclRelativeHumidityMeasurementCluster.CLUSTER_ID)
+                {
+                    var measuredValue = reportAttributesCommand.Reports.FirstOrDefault(
+                        o => o.AttributeIdentifier == ZclRelativeHumidityMeasurementCluster.ATTR_MEASUREDVALUE);
+                    if (measuredValue != null && measuredValue.AttributeDataType.DataType == DataType.UNSIGNED_16_BIT_INTEGER)
+                    {
+                        var value = Convert.ToDouble(measuredValue.AttributeValue);
+                        Console.WriteLine($"{DateTime.Now} {value / 100} %");
+                        return;
+                    }
+                }
+                else if (reportAttributesCommand.ClusterId == ZclTemperatureMeasurementCluster.CLUSTER_ID)
+                {
+                    var measuredValue = reportAttributesCommand.Reports.FirstOrDefault(
+                        o => o.AttributeIdentifier == ZclTemperatureMeasurementCluster.ATTR_MEASUREDVALUE);
+                    if (measuredValue != null && measuredValue.AttributeDataType.DataType == DataType.SIGNED_16_BIT_INTEGER)
+                    {
+                        var value = Convert.ToDouble(measuredValue.AttributeValue);
+                        Console.WriteLine($"{DateTime.Now} {value / 100} °C");
+                        return;
+                    }
+                }
+
                 Console.WriteLine(DateTime.Now);
                 Console.WriteLine(reportAttributesCommand);
                 foreach (var report in reportAttributesCommand.Reports)
